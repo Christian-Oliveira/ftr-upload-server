9fea018e3539bbc700cb955a93d66cf870ede39b
Christian-Oliveira <christianoliveirati@gmail.com>
2025-03-24 23:39:49 -0300

    feat: add rota de listagem de uploads

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -10,6 +10,7 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import { getUploadsRoute } from './routes/get-uploads'
 import { transformSwaggerSchema } from './transform-swagger-schema'
 
 const server = fastify()
@@ -48,6 +49,7 @@ server.register(fastifySwaggerUi, {
 })
 
 server.register(uploadImageRoute)
+server.register(getUploadsRoute)
 
 server.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log(`HTTP server listening on port ${env.PORT}`)
